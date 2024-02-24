7a1f7b5ca9b62af5cdde111f32723136e1dae0fb
Jack-Patterson <jackpatterson.10.02@gmail.com>
2024-02-24 18:54:30 +0000

    Working game need to limit depth of ply depth

--- Scripts/SearchTree/CA/CurrentBoard.py
+++ Scripts/SearchTree/CA/CurrentBoard.py
@@ -21,65 +21,88 @@ class Board:
             self.board = string_def
         self.state = self.state_of_board()
 
-    def display(self, game_display=False):
+    def state_of_board(self):
+        white_pieces, black_pieces = self.count_pieces()
+        white_moves = len(self.get_possible_moves('W'))
+        black_moves = len(self.get_possible_moves('B'))
 
-        if game_display:
-            ind = 0
-            new_board = ""
-            for char in self.board:
-                if char == " ":
-                    new_board += str(ind)
-                else:
-                    new_board += char
-                ind += 1
-        else:
-            new_board = self.board
+        if white_pieces == 0 and black_pieces == 0:
+            return 'D'
+        elif white_pieces == 0 or black_pieces == 0:
+            return 'W'
+
+        if white_moves == 0 or black_moves == 0:
+            return 'W'
+
+        return "U"
+
+    def display(self, highlight_positions=None):
 
         print("  A B C D E F G H")
         row_number = 8
-        for row in new_board:
-            print(f"{row_number} {' '.join(row)}")
+        for row_idx, row in enumerate(self.board):
+            display_row = ""
+            for col_idx, piece in enumerate(row):
+                if highlight_positions is not None and (row_idx, col_idx) in highlight_positions:
+                    display_row += 'o '
+                else:
+                    display_row += f"{piece} "
+            print(f"{row_number} {display_row}")
             row_number -= 1
+        print("\n")
 
     def other(self, piece):
         return 'B' if piece.upper() == 'W' else 'W'
-
-    def Eq3(self, i1, i2, i3):
-        if self.board[i1] == " ":
-            return False
-        return (self.board[i1] == self.board[i2]) and (self.board[i2] == self.board[i3])
-
-    def state_of_board(self):
-        # if self.Eq3(0, 1, 2) or self.Eq3(0, 3, 6) or self.Eq3(0, 4, 8):
-        #     return self.board[0]
-        # if self.Eq3(3, 4, 5) or self.Eq3(1, 4, 7) or self.Eq3(2, 4, 6):
-        #     return self.board[4]
-        # if self.Eq3(6, 7, 8) or self.Eq3(2, 5, 8):
-        #     return self.board[8]
-        #
-        # if " " in self.board:
-        #     return "U"
-        # return "D"
-        return "U"
 
     def all_possible_moves(self, player_piece: str):
         possible_moves = []
         no_piece_in_position = '.'
         direction = -1 if player_piece.upper() == 'W' else 1
-        start_row, end_row = (0, 7) if direction == -1 else (7, 0)
 
         for row in range(len(self.board)):
             for col in range(len(self.board[row])):
-                if self.is_player_piece(row, col, player_piece):
-                    self.find_moves_for_piece(player_piece, row, col, direction, possible_moves, no_piece_in_position, start_row,
-                                              end_row)
+                if self.is_player_piece_at_position(row, col, player_piece):
+                    self.find_moves_for_piece(player_piece, row, col, direction, possible_moves, no_piece_in_position)
 
         return possible_moves
 
-    def is_player_piece(self, row, col, player_piece):
+    def position_possible_moves(self, row, col, player_piece):
+        possible_moves = set()
+        no_piece_in_position = '.'
+        direction = -1 if player_piece.upper() == 'W' else 1
+
+        move_positions = [(row + direction, col - 1), (row + direction, col + 1)]
+
+        for new_row, new_col in move_positions:
+            if 0 <= new_row < len(self.board) and 0 <= new_col < len(self.board[0]):
+                if self.board[new_row][new_col] == no_piece_in_position:
+                    possible_moves.add((new_row, new_col))
+
+        return possible_moves
+
+    def is_valid_move_for_position(self, piece_row_index, piece_column_index, suggested_row_index,
+                                   suggested_column_index, player_piece):
+        return True if ((suggested_row_index, suggested_column_index) in
+                        self.position_possible_moves(piece_row_index, piece_column_index, player_piece)) else False
+
+    def get_possible_moves(self, player_piece, specific_position=None):
+        if specific_position:
+            row_index, col_index = self.get_value_position_at_index(specific_position)
+            possible_moves = self.position_possible_moves(row_index, col_index, player_piece)
+        else:
+            possible_moves = set()
+            for row in range(len(self.board)):
+                for col in range(len(self.board[row])):
+                    if self.board[row][col].lower() == player_piece.lower():
+                        piece_moves = self.position_possible_moves(row, col, player_piece)
+                        possible_moves.update(piece_moves)
+
+        return possible_moves
+
+    def is_player_piece_at_position(self, row, col, player_piece):
         return self.board[row][col].lower() == player_piece.lower()
 
-    def find_moves_for_piece(self, player_piece, row, col, direction, possible_moves, no_piece, start_row, end_row):
+    def find_moves_for_piece(self, player_piece, row, col, direction, possible_moves, no_piece):
         opponent_piece = self.other(player_piece)
         move_positions = [(row + direction, col - 1), (row + direction, col + 1)]
 
@@ -96,22 +119,18 @@ class Board:
 
     def add_move(self, row, col, new_row, new_col, possible_moves):
         new_board = self.get_new_board(row, col, new_row, new_col)
-        new_board.display()
-        print("\n")
         possible_moves.append(new_board)
 
     def add_jump_move(self, row, col, over_row, over_col, jump_row, jump_col, possible_moves):
         new_board = self.get_new_board(row, col, jump_row, jump_col)
         new_board.remove_at_board_position(over_row, over_col)
-        new_board.display()
-        print("\n")
         possible_moves.append(new_board)
 
-    def get_value_position_at_index(self, position):
+    def get_value_position_at_index(self, position: str):
         column_index = ord(position[0].upper()) - ord('A')
 
         row_index = 8 - int(position[1])
-        return self.board[row_index][column_index]
+        return row_index, column_index
 
     def indices_to_board_position(self, row_index, column_index):
         column_letter = chr(column_index + ord('A'))
@@ -140,3 +159,16 @@ class Board:
         new_board.switch_positions(current_position, potential_move_position)
 
         return new_board
+
+    def count_pieces(self):
+        black_pieces = 0
+        white_pieces = 0
+
+        for row in self.board:
+            for cell in row:
+                if cell.upper() == 'W':
+                    white_pieces += 1
+                elif cell.upper() == 'B':
+                    black_pieces += 1
+
+        return white_pieces, black_pieces
--- Scripts/SearchTree/CA/Play.py
+++ Scripts/SearchTree/CA/Play.py
@@ -1,24 +1,62 @@
 from SearchTree.CA.CurrentBoard import Board
+from SearchTree.CA.SearchTreeNode import SearchTreeNode
 
 
 def play():
-    response = input("Do you wish to play first (y/n) ?")
-    players_turn = (response == "y")
+    game_is_finished = False
+
+    response = input("Do you wish to play first? White will go first. (y/n) ")
+    players_turn = (response.lower() == "y")
 
     cb = Board()
-    player_is_playing = 'W' if players_turn is True else 'B'
-
-    print(player_is_playing)
-    cb.display()
 
     # initial_position = input("Enter the initial position (e.g., C8): ")
     # move_position = input("Enter the move position (e.g., D7): ")
     # cb.switch_positions(initial_position, move_position)
     # cb.display()
 
-    cb.switch_positions('C3', 'C5')
+    while game_is_finished is False:
+        player_is_playing = 'W' if players_turn is True else 'B'
 
-    print(len(cb.all_possible_moves(player_is_playing)))
+        if players_turn:
+            possible_moves = cb.get_possible_moves(player_is_playing)
+            print("Current Board State: \nAll possible places to move are signified by an 'o'.")
+            cb.display(possible_moves)
+
+            row, column = 0, 0
+            while cb.board[row][column].upper() != player_is_playing.upper():
+                initial_position = input("Enter the initial position (e.g., C8): ")
+                row, column = cb.get_value_position_at_index(initial_position)
+
+            possible_moves = cb.get_possible_moves(player_is_playing, initial_position)
+            cb.display(possible_moves)
+
+            move_position = input("Enter the move position (e.g., D7): ")
+            initial_row, initial_column = cb.get_value_position_at_index(initial_position)
+            move_row, move_column = cb.get_value_position_at_index(move_position)
+
+            if cb.is_valid_move_for_position(initial_row, initial_column, move_row, move_column, player_is_playing):
+                cb.switch_positions(initial_position, move_position)
+        else:
+            search_tree = SearchTreeNode(cb, cb.other(player_is_playing))
+            search_tree.min_max_value()
+            cb = search_tree.children[-1].current_board
+            # pass
+
+        if cb.state != 'U':
+            if cb.state == "D":
+                print(" I cant believe it you drew!!")
+            else:
+                if players_turn:
+                    print("This is impossible!!!!")
+                else:
+                    print("Too easy loser!!!")
+
+            print("Final Board State:")
+            cb.display()
+            game_is_finished = True
+
+        players_turn = not players_turn
 
 
 if __name__ == '__main__':
